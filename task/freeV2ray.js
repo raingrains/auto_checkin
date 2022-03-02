ab5a004d819a9fa40c4c4bf3331b3745cffcb3ab
张炷远 <zhangzhuyuan@u-jy.cn>
2022-03-02 09:00:09 +0800

    '解开mfv2ray注释'

--- task/freeV2ray.js
+++ task/freeV2ray.js
@@ -3,7 +3,7 @@ const axios = require('axios')
 module.exports = {
   // freeV2ray的自动签到
   check_in: (cookie) => {
-    return axios.post('https://www.mfv2ray.top/user/checkin','', {
+    return axios.post('https://by.jxdy.top/user/checkin','', {
       headers: {
         cookie
       }
